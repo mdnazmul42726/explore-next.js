d7681a0fbd01bc7a0a6cb58ff63c4f062199ffb0
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-03 00:06:31 +0600

    explore Link components and metadata

--- src/app/(withLayout)/nafisa/page.js
+++ src/app/(withLayout)/nafisa/page.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const metadata = {
+    title: 'nafisa',
+    description: 'this is nafisa'
+}
+
 const Nafisa = () => {
     return (
         <div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Counter from '@/components/Counter';
+import Link from 'next/link';
 import React from 'react';
 
 const HomeComponent = () => {
@@ -6,6 +7,8 @@ const HomeComponent = () => {
     <div>
       <p className='text-red-600'>hello world</p>
       <Counter />
+     <Link href={'/nazmul'}> <p>Nazmul route</p></Link>
+     <Link href={'/nafisa'}><p>Nafisa route</p></Link>
     </div>
   );
 };
